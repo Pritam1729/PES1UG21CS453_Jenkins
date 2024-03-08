01b197597a9c331a2f6ef70cd57bccc6acfef08b
Pritam S Gurav <106548018+Pritam1729@users.noreply.github.com>
2024-03-08 22:59:20 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World PES1UG21CS453 hello world\n";
+  cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "webhook successfully enabled\n";
   return 0;
 }
